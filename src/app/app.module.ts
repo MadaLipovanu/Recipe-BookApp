618f27f06e89230078de6b02895fa8d52c4caa92
Mada Lipovanu <madalina-denisa.lipovanu@atos.net>
2022-02-16 12:56:58 +0200

    Initial app

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core.module';
+import { HeaderComponent } from './header/header.component';
+import { LoggingService } from './logging.service';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    SharedModule,
+    CoreModule
+  ],
+  // providers: [LoggingService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+   firebaseAPIKey: 'AIzaSyAsAyREo_2_W1G820wg0YIUi_RnrSkqi7Q'
+};
